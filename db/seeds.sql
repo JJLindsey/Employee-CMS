620b3c0b798d6799cf264fc7039f794defb7ff26
JJLindsey <jlindsey010@gmail.com>
2021-07-08 17:36:23 -0700

    create db schema

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO department ()
+VALUES
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ();
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ();
