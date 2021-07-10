cd2ec3ee54f76eeb9af4ff31ddbc137a08dc4d8b
JJLindsey <jlindsey010@gmail.com>
2021-07-10 16:08:13 -0700

    start to functionalize options

--- index.js
+++ index.js
@@ -13,3 +13,35 @@ function roles() {
         }
     })
 }
+
+//functionalize all options
+//view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+//WHEN I choose to view all departments
+//presented with a formatted table showing department names and department ids
+const init = () => {
+    inquirer.prompt([
+        {   
+            type: 'list',
+            name: 'viewAll',
+            message: 'What would you like to do?',
+            choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role'],
+        }
+    ]).then((answer) =>{
+        console.log(answer.viewAll)
+
+        if(answer.viewAll == 'View all Departments') {
+            //call department function
+            getDeptData();
+        } else if (answer.viewAll == 'View all Roles') {
+            
+        }
+    })
+}
+
+
+//select
+
+//insert
+
+//update
