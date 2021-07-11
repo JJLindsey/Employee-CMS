58c2d6eeef91e342e61f7a3236f803ba19ad604d
JJLindsey <jlindsey010@gmail.com>
2021-07-10 17:21:44 -0700

    inquirer options to create new

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-function roles() {
-    let sql = `SELECT * FROM roles ORDER BY id`;
-    // Get Data - Query DB
-    dbData.query(sql, (err, result) => {
-        if (err) throw err;
-        // Display Data
-        console.table(result)
-        //add a 4 loop or for each to get through all the info, read more about the queries on Mysql
-        for (let i = 0; i < roles.length; i++) {
-            result.forEach(row => {
-                console.table(row)
-            }).then(userPrompt())
-        }
-    })
-}
+const inquirer = require('inquirer');
+
+// function roles() {
+//     let sql = `SELECT * FROM roles ORDER BY id`;
+//     // Get Data - Query DB
+//     dbData.query(sql, (err, result) => {
+//         if (err) throw err;
+//         // Display Data
+//         console.table(result)
+//         //add a 4 loop or for each to get through all the info, read more about the queries on Mysql
+//         for (let i = 0; i < roles.length; i++) {
+//             result.forEach(row => {
+//                 console.table(row)
+//             }).then(userPrompt())
+//         }
+//     })
+// }
 
 //functionalize all options
 //view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
@@ -34,14 +36,103 @@ const init = () => {
             //call department function
             getDeptData();
         } else if (answer.viewAll == 'View all Roles') {
-            
+            //call roles function
+            getRolesData();
+        } else if(answer.viewAll == 'View all Employees'){
+            getEmployeeData();
+        } else if(answer.viewAll == 'Add a Department'){
+            addDept();
+        } else if(answer.viewAll == 'Add a Role') {
+            addRole();
+        } else if(answer.viewAll == 'Add an Employee'){
+            addEmployee();
+        } else {
+            updateEmployeeRole();
         }
     })
 }
 
+function getDeptData() {
+    let sql = `SELECT * FROM department ORDER BY id`;
+    // Get Data - Query DB
+    dbData.query(sql, (err, result) => {
+        if (err) throw err;
+        // Display Data
+        console.table(result)
+        //add a 4 loop or for each to get through all the info, read more about the queries on Mysql
+        for (let i = 0; i < roles.length; i++) {
+            result.forEach(row => {
+                console.table(row)
+            }).then(userPrompt())
+        }
+    })
+};
 
+function getRolesData() {
+    let sql = `SELECT * FROM roles ORDER BY id`;
+    // Get Data - Query DB
+    dbData.query(sql, (err, result) => {
+        if (err) throw err;
+        // Display Data
+        console.table(result)
+        //add a 4 loop or for each to get through all the info, read more about the queries on Mysql
+        for (let i = 0; i < roles.length; i++) {
+            result.forEach(row => {
+                console.table(row)
+            }).then(userPrompt())
+        }
+    })
+};
+
+function getEmployeeData() {
+    let sql = `SELECT * FROM employee ORDER BY id`;
+    // Get Data - Query DB
+    dbData.query(sql, (err, result) => {
+        if (err) throw err;
+        // Display Data
+        console.table(result)
+        //add a 4 loop or for each to get through all the info, read more about the queries on Mysql
+        for (let i = 0; i < roles.length; i++) {
+            result.forEach(row => {
+                console.table(row)
+            }).then(userPrompt())
+        }
+    })
+};
+
+//add a new department
+const addDept = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'departmentName',
+            message: 'Please add the new Department.',
+        }
+    ]).then
+}
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'roles',
+            message: 'Please input new role.',
+        }
+    ]).then
+}
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'employee name',
+            message: 'Please enter employee first name and last name',
+        }
+    ]).then
+} 
 //select
 
 //insert
 
 //update
+init();
